242fdc443975b892d51bd6c5a9c3b794e407ac20
ZarXD <banghads09@gmail.com>
2026-05-19 20:11:13 +0700

    fix: pin home-manager to release-25.11 (must match nixpkgs)

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
