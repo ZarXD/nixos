7bae27679070ea3c741a19aed72894f385ea4aeb
ZarXD <banghads09@gmail.com>
2026-05-19 20:03:58 +0700

    fix: home-manager release-26.05 branch doesnt exist yet, use main

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     # Home Manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
